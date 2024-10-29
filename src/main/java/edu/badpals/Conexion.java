409bb3c7b430cf3d0a299e4dd8db5d0194a660c9
Victor <victorsancheznogueira@gmail.com>
2024-10-29 12:11:51 +0100

    feat: deletePais bbdd

--- src/main/java/edu/badpals/Conexion.java
+++ src/main/java/edu/badpals/Conexion.java
@@ -1,4 +1,84 @@
 package edu.badpals;
 
+import edu.badpals.Main;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Conexion {
+    Connection conexion;
+
+    public Conexion() {
+        makeConexion();
+    }
+
+    private void makeConexion() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            this.conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/bbdd", "a23victorsn", "renaido");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void getPaises() {
+        try {
+            // Execute the query
+            Statement s = conexion.createStatement();
+            ResultSet rs = s.executeQuery("SELECT * FROM Paises");
+            List<Pais> paises = new ArrayList<>();
+            while (rs.next()) {
+                Pais pais = new Pais(rs.getString(2),rs.getInt(3),rs.getString(4),rs.getString(5));
+                pais.setId(rs.getInt(1));
+                paises.add(pais);
+                System.out.println(pais);
+            }
+
+            // Close the resources
+            rs.close();
+            s.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public boolean insertarPais(Pais pais){
+        try{
+            PreparedStatement s = conexion.prepareStatement("INSERT INTO Paises (nombre_pais, numero_habitantes, nombre_capital, nombre_moneda)  " +
+                    " VALUES (?,?,?,?)");
+            s.setString(1,pais.getNombre_pais());
+            s.setInt(2,pais.getNumero_habitantes());
+            s.setString(3,pais.getNombre_capital());
+            s.setString(4,pais.getNombre_moneda());
+            s.executeUpdate();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+
+    public void cerrarConexion(){
+        try{
+            this.conexion.close();
+        } catch (SQLException e) {
+            e.getErrorCode();
+        }
+    }
+
+    public void borrarPais(int id) {
+        try{
+            PreparedStatement s = conexion.prepareStatement("DELETE FROM Paises WHERE id = ?");
+            s.setInt(1,id);
+            s.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/edu/badpals/Pais.java
+++ src/main/java/edu/badpals/Pais.java
@@ -1,4 +1,67 @@
 package edu.badpals;
 
+import java.math.BigInteger;
+
 public class Pais {
+    int id = 0;
+    String nombre_pais;
+    int numero_habitantes;
+    String nombre_capital;
+    String nombre_moneda;
+
+    public Pais(String nombre_pais, int numero_habitantes, String nombre_capital, String nombre_moneda) {
+        this.nombre_pais = nombre_pais;
+        this.numero_habitantes = numero_habitantes;
+        this.nombre_capital = nombre_capital;
+        this.nombre_moneda = nombre_moneda;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre_pais() {
+        return nombre_pais;
+    }
+
+    public void setNombre_pais(String nombre_pais) {
+        this.nombre_pais = nombre_pais;
+    }
+
+    public int getNumero_habitantes() {
+        return numero_habitantes;
+    }
+
+    public void setNumero_habitantes(int numero_habitantes) {
+        this.numero_habitantes = numero_habitantes;
+    }
+
+    public String getNombre_capital() {
+        return nombre_capital;
+    }
+
+    public void setNombre_capital(String nombre_capital) {
+        this.nombre_capital = nombre_capital;
+    }
+
+    public String getNombre_moneda() {
+        return nombre_moneda;
+    }
+
+    public void setNombre_moneda(String nombre_moneda) {
+        this.nombre_moneda = nombre_moneda;
+    }
+
+    @Override
+    public String toString() {
+        return "id=" + id +
+                ", nombre_pais='" + nombre_pais + '\'' +
+                ", numero_habitantes=" + numero_habitantes +
+                ", nombre_capital='" + nombre_capital + '\'' +
+                ", nombre_moneda='" + nombre_moneda;
+    }
 }
